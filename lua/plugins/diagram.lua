6a584b8d8d260ae95c17ac23ee0cb0ec62065858
jake <jakehillgithub@gmail.com>
2025-11-17 12:51:47 -0700

    Update diagram settings to display in seperate tab/buffer

--- lua/plugins/diagram.lua
+++ lua/plugins/diagram.lua
@@ -3,4 +3,27 @@ return {
   dependencies = {
     { "3rd/image.nvim", opts = {} },
   },
+  opts = {
+    events = {
+      render_buffer = {}, -- Empty = no automatic rendering
+      clear_buffer = { "BufLeave" },
+    },
+    renderer_options = {
+      mermaid = {
+        theme = "dark",
+        scale = 2,
+      },
+    },
+  },
+  keys = {
+    {
+      "K", -- or any key you prefer
+      function()
+        require("diagram").show_diagram_hover()
+      end,
+      mode = "n",
+      ft = { "markdown", "norg" }, -- Only in these filetypes
+      desc = "Show diagram in new tab",
+    },
+  },
 }
