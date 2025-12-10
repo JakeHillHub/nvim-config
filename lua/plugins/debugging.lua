84be3377012598e27bdcdf2513a58d893b2fed37
jake <jake@genevatech.net>
2025-12-10 16:17:41 -0700

    dap ui doesn't open automatically

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -12,9 +12,6 @@ return {
 
       dapui.setup()
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
       dap.listeners.before.launch.dapui_config = function()
         dapui.open()
       end
